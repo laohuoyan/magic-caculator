a545edb4c9f6f03840ec2edf38934197dc24bbfb
Lim <oncwnuNmqDLfBRgceXWI_G4BisFw@git.weixin.qq.com>
2023-10-11 20:26:15 +0800

    feat: 初始布局

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -4,18 +4,64 @@ const app = getApp<IAppOption>()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    // 表达式
+    expression: '',
+    // 目前为止的计算结果
+    curResult: 0,
+    // 当前的操作符（加减乘除）
+    curOperator: '+',
+    // 正在输入的内容
+    curInputStr: '',
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs',
+
+  //
+  //
+  //
+
+  equal() {
+    const { curResult, curInputStr } = this.data;
+    const result = curResult + Number(curInputStr);
+
+    this.setData({
+      curResult: result,
+      curInputStr: String(result),
     })
+  },
+
+  reset() {
+    this.setData({
+      curInputStr: "0",
+      curResult: 0,
+      curOperator: '+'
+    })
+  },
+
+  //
+  //  回调函数
+  //
+  
+  onClick(event: any)  {
+    console.log(event);
+    const value = event.target.id;
+    switch (value) {
+      case '+':
+      case '-':
+      case 'x':
+      case '/':
+        this.equal();
+        this.setData({
+          curOperator: value,
+        });
+        break;
+      case '=':
+        this.equal();
+        break;
+      default:
+        console.log('value ->', value);
+        this.setData({
+          curInputStr: this.data.curInputStr + value
+        })
+    }
   },
   onLoad() {
     // @ts-ignore
@@ -25,25 +71,4 @@ Page({
       })
     }
   },
-  getUserProfile() {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e: any) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
