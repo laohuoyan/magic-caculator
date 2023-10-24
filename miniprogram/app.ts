4770bc0f71004b3e721ae44ebcba3b913a251ba2
Lim <oncwnuNmqDLfBRgceXWI_G4BisFw@git.weixin.qq.com>
2023-10-24 19:43:19 +0800

    feat: 移除无用代码

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -2,11 +2,6 @@
 App<IAppOption>({
   globalData: {},
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
     // 登录
     wx.login({
       success: res => {
